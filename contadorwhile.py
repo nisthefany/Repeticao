d928b3b428e57b8ad995ddcde129db8d0d56fa3f
nisthefany <116585969+nisthefany@users.noreply.github.com>
2023-01-20 14:19:35 -0300

    repeticao

---
+++ contadorwhile.py
@@ -0,0 +1,13 @@
+# contadores com while
+x=1
+print("Contador progressivo: ")
+while x<=10:
+    print(x)
+    x+=1
+
+print("Contador regressivo: ")
+y=10
+while y>=1:
+    print(y)
+    y-=1
+    
